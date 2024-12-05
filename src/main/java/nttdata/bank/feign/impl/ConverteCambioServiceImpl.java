3011884d77b814d8951f44f80e2e17a0aa270410
Guilherme Rodrigues <ext.ggomes@unicid.edu.br>
2024-12-05 19:44:09 -0300

    feat: cria cache para cotacao externa

--- src/main/java/nttdata/bank/feign/impl/ConverteCambioServiceImpl.java
+++ src/main/java/nttdata/bank/feign/impl/ConverteCambioServiceImpl.java
@@ -6,6 +6,7 @@ import nttdata.bank.feign.response.cambio.CambioResponse;
 import nttdata.bank.service.ports.ConverteCambioService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -24,21 +25,46 @@ public class ConverteCambioServiceImpl implements ConverteCambioService {
     }
 
     @Override
+    @Cacheable(value = "cotacaoCache", key = "#moedaOrigem")
+    public Optional<?> buscarTaxaCambio(String moedaOrigem) {
+        log.info("Obtendo taxa de cambio atualizada (Base em EUR)...");
+        try {
+
+            Optional<CambioResponse> cambioResponse = converteCambioClient.getTodasCotacoes();
+            if (cambioResponse.isPresent()) {
+                log.info("Cotacoes recebidas com sucesso...");
+
+                BigDecimal taxaOrigem = cambioResponse.get().getCotacaoMoeda(moedaOrigem);
+                if (cotacaoValida(taxaOrigem)) {
+                    return Optional.of(taxaOrigem.setScale(2, RoundingMode.HALF_UP));
+                } else {
+                    log.warn("Nao foi possivel converter a moeda: Cotacao invalida!");
+                    throw new CambioBusinessException("Nao foi possivel converter a moeda: Cotacao invalida!");
+                }
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            log.error("Erro ao chamar api para conversao de cambio...");
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
     public Optional<?> converterCambio(String moedaOrigem, String moedaDestino, BigDecimal valor) {
         log.info("Chamando api externa para conversao de cambio...");
-        try{
-            if(moedaOrigem.equals(moedaDestino)){
+        try {
+            if (moedaOrigem.equals(moedaDestino)) {
                 log.warn("Moedas de origem e destino iguais, nao e necessario conversao");
                 return Optional.of(valor);
             }
 
-            if(valor.compareTo(BigDecimal.ZERO) <= 0){
+            if (valor.compareTo(BigDecimal.ZERO) <= 0) {
                 log.warn("Valor invalido para conversao");
                 throw new CambioBusinessException("Valor invalido para conversao");
             }
 
             Optional<CambioResponse> cambioResponse = converteCambioClient.getTodasCotacoes();
-            if(cambioResponse.isPresent()){
+            if (cambioResponse.isPresent()) {
                 log.info("Cotacoes recebidas com sucesso, realizando conversao de {} para {} ...", moedaOrigem, moedaDestino);
 
                 BigDecimal taxaOrigem = cambioResponse.get().getCotacaoMoeda(moedaOrigem);
