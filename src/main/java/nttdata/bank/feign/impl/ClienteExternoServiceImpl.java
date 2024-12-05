3d4d08aaefcd022a0787876480a80ebb7074c9ad
Guilherme Rodrigues <ext.ggomes@unicid.edu.br>
2024-12-05 19:49:42 -0300

    feat: cria endpoint mockapi saldo cliente externo

--- src/main/java/nttdata/bank/feign/impl/ClienteExternoServiceImpl.java
+++ src/main/java/nttdata/bank/feign/impl/ClienteExternoServiceImpl.java
@@ -5,6 +5,9 @@ import nttdata.bank.feign.response.mockapi.ClienteExterno;
 import nttdata.bank.service.ports.ClienteExternoService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,10 @@ public class ClienteExternoServiceImpl implements ClienteExternoService {
     @Override
     public Optional<ClienteExterno> buscarClienteExterno() {
         return contaExternaClient.getContaClieneExterno();
+    }
+
+    @Override
+    public Optional<BigDecimal> buscarSaldoClienteExterno(String idClienteExterno) {
+        return contaExternaClient.getSaldoClieneExterno(idClienteExterno).stream().map(saldo -> saldo.get("saldo")).findFirst();
     }
 }
