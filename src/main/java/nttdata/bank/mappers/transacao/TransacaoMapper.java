c442ff6711c87da640759e0ff071601478f43d8b
Guilherme Rodrigues <ext.ggomes@unicid.edu.br>
2024-12-12 09:27:23 -0300

    feat: cria relatorio transacoes

--- src/main/java/nttdata/bank/mappers/transacao/TransacaoMapper.java
+++ src/main/java/nttdata/bank/mappers/transacao/TransacaoMapper.java
@@ -2,9 +2,14 @@ package nttdata.bank.mappers.transacao;
 
 import nttdata.bank.controllers.transacao.requests.TransacaoRequest;
 import nttdata.bank.controllers.transacao.responses.TransacaoResponse;
+import nttdata.bank.domain.dto.transacao.TransacaoDTO;
+import nttdata.bank.domain.entities.conta.Conta;
 import nttdata.bank.domain.entities.transacao.Transacao;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TransacaoMapper {
@@ -17,5 +22,10 @@ public interface TransacaoMapper {
     Transacao toTransacao(TransacaoRequest request);
 
     TransacaoResponse toTransacaoResponse(Transacao transacao);
+
+    @Mapping(target = "contaOrigem", ignore = true)
+    @Mapping(target = "contaDestino", ignore = true)
+    List<TransacaoDTO> toListaTransacaoDTO(List<Transacao> transacoes);
+
 
 }
--- src/main/java/nttdata/bank/repository/transacao/TransacaoRepository.java
+++ src/main/java/nttdata/bank/repository/transacao/TransacaoRepository.java
@@ -2,8 +2,16 @@ package nttdata.bank.repository.transacao;
 
 import nttdata.bank.domain.entities.transacao.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+
+    @Query("SELECT t FROM Transacao t WHERE t.contaOrigem.usuario.id = :idClienteContaOrigem")
+    Optional<List<Transacao>> findByIdClienteContaOrigem(Long idClienteContaOrigem);;
 }
